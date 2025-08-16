2fa89c664adaae9826f79e172b64562aee1c4d67
AdamNurlign <adamnurl11@gmail.com>
2025-08-16 15:09:50 -0400

    Neural Network Application v1
    Hello! This is a neural network application with a backend implemented primarily using NumPy and a frontend GUI built using Tkinter. For more details, please see the README file. Thank you!

---
+++ main.py
@@ -0,0 +1,20 @@
+from tkinter import *
+from tkinter import ttk
+import CreateNeuralNetwork as cnn
+
+
+#create Tk object called root- the main application window
+root=Tk()
+
+#pass in the main application window into the CreateNeuralNetwork class constructor
+#to create the widgets that will run in the main application window and get 
+#access to them in the constructor
+CreateNeuralNetworkApp=cnn.CreateNeuralNetwork(root)
+
+
+#Tell tk to enter its event loop
+root.mainloop()
+
+
+
+
